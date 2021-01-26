9909d2c2396c553bca5f3fe65f6575c70630f1e1
su3aki <su3aki.08@gmail.com>
2021-01-26 17:39:19 +0900

    0126part2compALL

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import './index.css'
+
+  //Footer仮 </Footer>で表示
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, MADE BY おれ</em>
+    </div>
+  )
+}
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
 
 //ここでJsonサーバーからデータを受け取る
@@ -27,7 +45,7 @@ const App = () => {
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
     }
-
+    //ここで新しいノートオブジェクトを設定
     noteService
     .create(noteObject)
     .then(returnedNote => {
@@ -46,9 +64,12 @@ const App = () => {
       setNotes(notes.map(note => note.id !== id ? note : returnedNote))
     })
     .catch(error => {
-      alert(
-        `the note '${note.content}' was already deleted from server`
-      )
+       setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
       setNotes(notes.filter(n => n.id !== id))
     })
 }
@@ -63,9 +84,23 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important === true)
 
+  //エラーメッセージ
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+
+    return (
+      <div className="error">
+        {message}
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? '神' : '全部' }
@@ -87,6 +122,7 @@ const App = () => {
         />
         <button type="submit">決定</button>
       </form>
+      <Footer />
     </div>
   )
 }
