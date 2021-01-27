4d311ce6cce4b37c4c88fbdb6b3d4cbcf35ccb27
su3aki <su3aki.08@gmail.com>
2021-01-27 23:33:32 +0900

    0127part3compA

--- part3/index.js
+++ part3/index.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -30,6 +31,34 @@ let notes = [
 app.post('/api/notes', (request, response) => {
   const note = request.body
   console.log(note)
+
+  response.json(note)
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
 
   response.json(note)
 })
