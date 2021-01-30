28d113525001b2dbbb356e41d3a19a9fbf014823
su3aki <su3aki.08@gmail.com>
2021-01-30 23:03:47 +0900

    0130part3compB

--- part3/index.js
+++ part3/index.js
@@ -5,9 +5,9 @@ const cors = require('cors')
 
 //MongoDBに接続　始
 const mongoose = require('mongoose')
-
+const password =  process.argv[2]
 const url =
-  ''
+  `mongodb+srv://KiyomaroDev:${password}@cluster0.jdqnx.mongodb.net/fsoReal?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
@@ -25,6 +25,27 @@ app.use(express.static('build'))
 
 //corsを使ってフロントサイドのservices/note.jsにデータを運ぶ
 app.use(cors())
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
+]
 
 //PostされたデータをExpressがJSONで処理する
 app.use(express.json())
