877d1dc43d10ff261ea3f03c00f5df6afb30887a
su3aki <su3aki.08@gmail.com>
2021-02-09 16:18:27 +0900

    part4start

--- part3/index.js
+++ part3/index.js
@@ -1,10 +1,112 @@
-const app = require('./app') // the actual Express application
-const http = require('http')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const Note = require('./models/note')
+const cors = require('cors')
 
-const server = http.createServer(app)
+//corsを使ってフロントサイドのservices/note.jsにデータを運ぶ
+app.use(cors())
 
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+app.use(express.static('build'))
+
+//PostされたデータをExpressがJSONで処理する
+app.use(express.json())
+
+//MongoDBに記録
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  note
+    .save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => {
+      response.json(savedAndFormattedNote)
+    })
+    .catch(error => next(error))
+})
+
+
+//app.getでバックエンドサーバーのルートを設定de
+
+app.get('/', (request, response) => {
+  response.send('<h1>こんにちわあああ</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+//ルーティングエラーの記述
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'このURLに関連するデータが紐付けされていません' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: '指定されたIDのMongoオブジェクトは存在しません' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+//サーバーのポート設定
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
