46c5a16815ec14c454f34ec3230cbf8a331f4372
su3aki <su3aki.08@gmail.com>
2021-02-21 00:49:23 +0900

    0221part5stillB

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,35 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
+import Notification from './components/Notification'
+import NoteForm from './components/NoteForm'
+import LoginForm from './components/LoginForm'
 import noteService from './services/notes'
 import loginService from './services/login'
-import LoginForm from './components/LoginForm'
+import Togglable from './components/Togglable'
+import Footer from './components/Footer'
 import './index.css'
-
-  //Footer仮 </Footer>で表示
-
-const Footer = () => {
-  const footerStyle = {
-    color: 'green',
-    fontStyle: 'italic',
-    fontSize: 16
-  }
-  return (
-    <div style={footerStyle}>
-      <br />
-      <em>Note app, MADE BY おれ</em>
-    </div>
-  )
-}
 
 const App = () => {
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
-  const [loginVisible, setLoginVisible] = useState(false)
 
 //ここでJsonサーバーからデータを受け取る
   useEffect(() => {
@@ -52,121 +38,89 @@ const App = () => {
 
 
   //投稿をjason DBに記録
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-    }
+  const addNote = (noteObject) => {
     //ここで新しいノートオブジェクトを設定
     noteService
     .create(noteObject)
     .then(returnedNote => {
       setNotes(notes.concat(returnedNote))
-      setNewNote('')
     })
   }
 
-  //重要パラメータの変更ボタンの動作
-    const toggleImportanceOf = (id) => {
-      const note = notes.find(n => n.id === id)
-      const changedNote = { ...note, important: !note.important }
-
-      noteService
-    .update(id, changedNote).then(returnedNote => {
+//重要パラメータの変更ボタンの動作
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
       setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-    })
-    .catch(error => {
-      setErrorMessage(
-          `Note '${note.content}' was already removed from server`
-        )
-        setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-      setNotes(notes.filter(n => n.id !== id))
-    })
+  })
+  .catch(error => {
+    setErrorMessage(
+        `Note '${note.content}' was already removed from server`
+    )
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  })
 }
-  //form中身の変更内容取得
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
+//ログイン機構
+const handleLogin = async (event) => {
+  event.preventDefault()
+  try {
+    const user = await loginService.login({
+      username, password,
+    })
+    window.localStorage.setItem(
+      'loggedNoteappUser', JSON.stringify(user)
+    )
+    noteService.setToken(user.token)
+    setUser(user)
+    setUsername('')
+    setPassword('')
+  } catch (exception) {
+    setErrorMessage('Wrong credentials')
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
   }
+  console.log('logging in with', username, password)
+}
 
-  //重要項目だけ表示するか選択
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important === true)
-
-  //エラーメッセージ
-  const Notification = ({ message }) => {
-    if (message === null) {
-      return null
-    }
-
-    return (
-      <div className="error">
+//エラーメッセージ
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+  return (
+    <div className="error">
         {message}
       </div>
     )
   }
-  //ログイン機構
-  const handleLogin = async (event) => {
-    event.preventDefault()
-
-    try {
-      const user = await loginService.login({
-        username, password,
-      })
-
-      window.localStorage.setItem(
-        'loggedNoteappUser', JSON.stringify(user)
-      )
-      noteService.setToken(user.token)
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (exception) {
-      setErrorMessage('Wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-    }
-    console.log('logging in with', username, password)
-  }
   //ログインフォーム
-  const loginForm = () => {
-    const hideWhenVisible = { display: loginVisible ? 'none' : '' }
-    const showWhenVisible = { display: loginVisible ? '' : 'none' }
-    //三項演算子によってボタンの出現を管理
-    return (
-      <div>
-        <div style={hideWhenVisible}>
-          <button onClick={() => setLoginVisible(true)}>ログインはここから</button>
-        </div>
-        <div style={showWhenVisible}>
-          <LoginForm
-            username={username}
-            password={password}
-            handleUsernameChange={({ target }) => setUsername(target.value)}
-            handlePasswordChange={({ target }) => setPassword(target.value)}
-            handleSubmit={handleLogin}
-          />
-          <button onClick={() => setLoginVisible(false)}>キャンセル</button>
-        </div>
-      </div>
-    )
-  }
+  const loginForm = () => (
+    <Togglable buttonLabel='login'>
+      <LoginForm
+        username={username}
+        password={password}
+        handleUsernameChange={({ target }) => setUsername(target.value)}
+        handlePasswordChange={({ target }) => setPassword(target.value)}
+        handleSubmit={handleLogin}
+        />
+    </Togglable>
+  )
   //ノートフォーム
   const noteForm = () => (
-    <form onSubmit={addNote}>
-      <input
-        value={newNote}
-        onChange={handleNoteChange}
-      />
-      <button type="submit">save</button>
-    </form>
+    <Togglable buttonLabel='new note'>
+      <NoteForm createNote={addNote} />
+    </Togglable>
   )
+  //重要項目だけ表示するか選択
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
 
   //条件付きレンダリングの一貫　
   return (
@@ -177,11 +131,10 @@ const App = () => {
       {user === null ?
       loginForm() :
       <div>
-        <p>{user.name} logged-in</p>
+        <p>{user.name} ログインしました</p>
         {noteForm()}
       </div>
       }
-
       <h2>Notes</h2>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
@@ -197,13 +150,7 @@ const App = () => {
           />
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">決定</button>
-      </form>
+
       <Footer />
     </div>
   )
