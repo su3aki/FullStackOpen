fc4512b0478ce2fc6acff3d1fd8e1ee08dd77d87
su3aki <su3aki.08@gmail.com>
2021-04-08 00:06:38 +0900

    Add part9c

--- part9/index.ts
+++ part9/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import { calculator } from './calculator'
 const app = express();
 
-app.get('/ping', (req, res) => {
+app.get('/ping', (_req, res) => {
   res.send('pong');
 });
 
+app.post('/calculate', (req, res) => {
+  const { value1, value2, op } = req.body
+  let value3: any = 1
+  const result = calculator(value1, value2, op);
+  res.send(result);
+})
 const PORT = 3003;
 
 app.listen(PORT, () => {
